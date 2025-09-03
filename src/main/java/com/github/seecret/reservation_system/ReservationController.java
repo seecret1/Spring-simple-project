367ccb0ba833ee427fe851a3734bc390b9d10caa
seecret <naintonight@gmail.com>
2025-09-03 23:35:24 +0300

    Create post, put, delete, update http method. Need finish it http method approve delete reservation.

--- src/main/java/com/github/seecret/reservation_system/ReservationController.java
+++ src/main/java/com/github/seecret/reservation_system/ReservationController.java
@@ -1,12 +1,20 @@
 package com.github.seecret.reservation_system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/reservation")
 public class ReservationController {
+
+    private final static Logger log = LoggerFactory.getLogger(ReservationController.class);
 
     private final ReservationService reservationService;
 
@@ -16,11 +24,63 @@ public class ReservationController {
     }
 
     @GetMapping("/{id}")
-    public Reservation getReservationById(
+    public ResponseEntity<Reservation> getReservationById(
             @PathVariable("id") Long id
         ) {
-        System.out.println("log called method");
-        return reservationService.getReservationById(id);
+        log.info("Called getReservationById id = {}", id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).
+                    body(reservationService.getReservationById(id));
+        } catch(NoSuchElementException e) {
+            return ResponseEntity.status(404).build();
+        }
     }
 
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservation() {
+        log.info("Called getAllReservation") ;
+        return ResponseEntity.ok(reservationService.findAllReservation());
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(
+            @RequestBody Reservation reservationToCreate
+    ) {
+        log.info("Called createReservation");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(reservationService.createReservation(reservationToCreate));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Reservation> updateReservation(
+            @PathVariable Long id,
+            @RequestBody Reservation reservationToUpdate
+    ) {
+        log.info("Called updateReservation id={}, reservationToUpdate = {}",
+                id, reservationToUpdate);
+        var updated = reservationService.updateReservation(id, reservationToUpdate);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(
+            Long id
+    ) {
+        log.info("Called deleteReservation id={}", id);
+        try {
+            reservationService.deleteReservation(id);
+            return ResponseEntity.ok().build();
+        } catch(NoSuchElementException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @PostMapping("/{id}/approve")
+    public ResponseEntity<Reservation> approveReservation(
+            @PathVariable("id") Long id
+    ) {
+        log.info("Called approceReservation: id={}", id);
+        var reservation = reservationService.approveReservation(id);
+        return ResponseEntity.ok(reservation);
+    }
 }
--- src/main/java/com/github/seecret/reservation_system/ReservationService.java
+++ src/main/java/com/github/seecret/reservation_system/ReservationService.java
@@ -3,18 +3,104 @@ package com.github.seecret.reservation_system;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ReservationService {
 
+    private final Map<Long, Reservation> reservationMap;
+
+    private final AtomicLong idCounter;
+
+    public ReservationService() {
+        reservationMap = new HashMap<>();
+        idCounter = new AtomicLong();
+    }
+
     public Reservation getReservationById(Long id) {
-        return new Reservation(
-                id,
-                100L,
-                40L,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5),
-                ReservationStatus.APPROVED
-                );
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        }
+        return reservationMap.get(id);
+    }
+
+
+    public List<Reservation> findAllReservation() {
+        return reservationMap.values().stream().toList();
+    }
+
+    public Reservation createReservation(Reservation reservationToCreate) {
+        if (reservationToCreate.id() != null)
+            throw new IllegalArgumentException("Id should be empty");
+
+        if (reservationToCreate.status() != null)
+            throw new IllegalArgumentException("Status should be empty");
+
+        var newReservation = new Reservation(
+                idCounter.incrementAndGet(),
+                reservationToCreate.userId(),
+                reservationToCreate.roomId(),
+                reservationToCreate.startDate(),
+                reservationToCreate.endDate(),
+                ReservationStatus.PENDING
+        );
+
+        reservationMap.put(newReservation.id(), newReservation);
+        return newReservation;
+    }
+
+    public Reservation updateReservation(
+            Long id,
+            Reservation reservationToUpdate
+    ) {
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        }
+
+        var reservation = reservationMap.get(id);
+        if (reservation.status() != ReservationStatus.PENDING) {
+            throw new IllegalStateException("Cannot modify reservation: status=" + reservation.status());
+        }
+
+        var updatedReservation = new Reservation(
+                idCounter.incrementAndGet(),
+                reservationToUpdate.userId(),
+                reservationToUpdate.roomId(),
+                reservationToUpdate.startDate(),
+                reservationToUpdate.endDate(),
+                ReservationStatus.PENDING
+        );
+
+        reservationMap.put(updatedReservation.id(), updatedReservation);
+        return updatedReservation;
+    }
+
+    public void deleteReservation(Long id) {
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        }
+
+        reservationMap.remove(id);
+    }
+
+    public Reservation approveReservation(Long id) {
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        }
+
+        var reservation = reservationMap.get(id);
+        if (reservation.status() != ReservationStatus.PENDING) {
+            throw new IllegalStateException("Cannot approve reservation: status=" + reservation.status());
+        }
+    }
+
+    private boolean isReservationConflict(
+            Reservation reservation
+    ) {
+
     }
 }
